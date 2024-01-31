6850c1e453cb1b437028c18da0eec4c5c452aa79
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2024-01-31 13:50:06 +0530

    admin page done

--- frontend/src/Components/Admin/Admin.jsx
+++ frontend/src/Components/Admin/Admin.jsx
@@ -1,7 +1,25 @@
+import Card from "../Card/Card";
+
 const Admin = () => {
   return (
     <>
-      <h1>Welcome, Admin ...</h1>
+      <p className="font-bold font-serif text-5xl p-5">
+        <h1>Hey!!! XYZ Admin </h1>
+      </p>
+      <div className="flex justify-center items-center">
+        <p className="text-3xl bg-gray-200 p-6 rounded-xl">
+          UPCOMING EVENTS <span className="text-sm">see all</span>
+        </p>
+      </div>
+      <div>
+        <Card name={"More details"} />
+      </div>
+      <div>
+        <Card name={"More details"} />
+      </div>
+      <div>
+        <Card name={"More details"} />
+      </div>
     </>
   );
 };
